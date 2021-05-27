d19d2b48386c3831fa4fbf26d6f13b3484cc3a6d
djksbm <48785200+djksbm@users.noreply.github.com>
2021-05-27 15:22:11 +0200

    M1

--- js/ratings.js
+++ js/ratings.js
@@ -7,9 +7,9 @@ function collect_ratings(){
     const elements= document.querySelectorAll('.rating');
     let rating = 0;
     elements.forEach(element => {
-        rating = Number.parseInt(element.id.replace('star',''));
-        ratings.count += 1;;
-        ratings.sum += Number.parseInt(element.value)*rating;
+        rating = parseInt(element.id.replace('star',''));
+        ratings.count += parseInt(element.value);
+        ratings.sum += parseInt(element.value)*rating;
     });
     if(ratings.count !== 0 ){
         ratings.average=ratings.sum/ratings.count;
